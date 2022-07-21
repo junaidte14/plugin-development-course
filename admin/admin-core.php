65fa07669d31dcbbca5f343a047f5a653c55039c
Junaid Hassan <junaidte14@gmail.com>
2022-07-21 16:04:38 +0500

    settings page

--- admin/admin-core.php
+++ admin/admin-core.php
@@ -43,8 +43,17 @@ function pluginprefix_menu_button() {
 
     add_submenu_page(
         'myplugin',
-        'My Submenu',
-        'My Submenu',
+        'Dashboard',
+        'Dashboard',
+        'manage_options',
+        'myplugin',
+        'pluginprefix_menu_content'
+    );
+
+    add_submenu_page(
+        'myplugin',
+        'Settings',
+        'Settings',
         'manage_options',
         'submenu',
         'pluginprefix_add_submenu_page_content'
@@ -52,9 +61,68 @@ function pluginprefix_menu_button() {
 }
 
 function pluginprefix_add_submenu_page_content(){
+    // check user capabilities
+    if ( !current_user_can( 'manage_options' ) ) {
+        return;
+    }
+
+    // WordPress will add the "settings-updated" $_GET parameter to the url
+    if ( isset( $_GET['settings-updated'] ) ) {
+        // add settings saved message with the class of "updated"
+        add_settings_error( 'pluginprefix_messages', 'pluginprefix_message', __( 'Settings Saved', 'pluginprefix' ), 'updated' );
+    }
+
+    // show error/update messages
+    settings_errors( 'pluginprefix_messages' );
     ?>
-    <h2>This is our submenu page</h2>
-    <p>Testing submenu functionality!</p>
+    <div class="wrap">
+        <form action="options.php" method="post">
+            <?php
+            // output security fields for the registered setting "submenu"
+            settings_fields( 'submenu' );
+            // output setting sections and their fields
+            // (sections are registered for "wporg", each field is registered to a specific section)
+            do_settings_sections( 'submenu' );
+            // output save settings button
+            submit_button( 'Save Settings' );
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
+//using settings API
+function pluginprefix_my_plugin_settings(){
+    // register a new setting for "submenu" page
+    register_setting('submenu', 'pluginprefix_my_custom_setting');
+ 
+    // register a new section in the "submenu" page
+    add_settings_section(
+        'pluginprefix_settings_section',
+        'My Plugin Settings Section', 'pluginprefix_settings_section_callback',
+        'submenu'
+    );
+ 
+    // register a new field in the "pluginprefix_settings_section" section, inside the "submenu" page
+    add_settings_field(
+        'pluginprefix_settings_field',
+        'My Plugin Setting', 'pluginprefix_settings_field_callback',
+        'submenu',
+        'pluginprefix_settings_section'
+    );
+}
+
+add_action('admin_init', 'pluginprefix_my_plugin_settings');
+
+function pluginprefix_settings_section_callback(){
+    ?>
+    <?php
+}
+
+function pluginprefix_settings_field_callback(){
+    $setting = get_option('pluginprefix_my_custom_setting');
+    ?>
+    <input type="text" name="pluginprefix_my_custom_setting" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
     <?php
 }
 
--- includes/common-core.php
+++ includes/common-core.php
@@ -40,9 +40,25 @@ add_action('pluginprefix_after_settings_page_html', 'pluginprefix_add_content_us
 function pluginprefix_custom_box_html($post){
     //var_dump($post);
     $current_book_author = get_post_meta( $post->ID, '_pluginprefix_book_author', true );
+
+    $current_book_type = get_post_meta( $post->ID, '_pluginprefix_book_type', true );
+
+    $current_book_downloadable = get_post_meta( $post->ID, '_pluginprefix_book_downloadable', true );
+
     ?>
     <label for="pluginprefix_book_author">Book Author Name:</label>
     <input name="pluginprefix_book_author" id="pluginprefix_book_author" value="<?php echo $current_book_author;?>" />
+    <br><br>
+    <label for="pluginprefix_book_type">The book is available in</label>
+    <select name="pluginprefix_book_type" id="pluginprefix_book_type">
+        <option value="" <?php selected($current_book_type, "");?>>Select Option</option>
+        <option value="pdf" <?php selected($current_book_type, "pdf");?>>PDF</option>
+        <option value="worddoc" <?php selected($current_book_type, "worddoc");?>>Word Document</option>
+        <option value="both" <?php selected($current_book_type, "both");?>>Both</option>
+    </select>
+    <br><br>
+    <label for="pluginprefix_book_downloadable">The Book is Downloadable?</label>
+    <input type="checkbox" name="pluginprefix_book_downloadable" id="pluginprefix_book_downloadable" <?php if($current_book_downloadable == 'on'){echo 'checked="checked"';}?>/>
     <?php
 }
 
@@ -57,6 +73,7 @@ function pluginprefix_add_meta_box(){
 add_action('add_meta_boxes', 'pluginprefix_add_meta_box');
 
 function pluginprefix_save_postdata( $post_id ) {
+    //var_dump($_POST['pluginprefix_book_downloadable']); die();
     if ( array_key_exists( 'pluginprefix_book_author', $_POST ) ) {
         update_post_meta(
             $post_id,
@@ -64,6 +81,23 @@ function pluginprefix_save_postdata( $post_id ) {
             $_POST['pluginprefix_book_author']
         );
     }
+
+    if ( array_key_exists( 'pluginprefix_book_type', $_POST ) ) {
+        update_post_meta(
+            $post_id,
+            '_pluginprefix_book_type',
+            $_POST['pluginprefix_book_type']
+        );
+    }
+
+    $current_checkbox_value = ($_POST['pluginprefix_book_downloadable']) ? $_POST['pluginprefix_book_downloadable'] : 'off';
+    //echo $current_checkbox_value; die();
+
+    update_post_meta(
+        $post_id,
+        '_pluginprefix_book_downloadable',
+        $current_checkbox_value
+    );
 }
 add_action( 'save_post', 'pluginprefix_save_postdata' );
 ?>
